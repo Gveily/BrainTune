a158eb15ce6ff9a838ced85d134b4527df017167
Mikita Kalashnikau <nikita.kalashnikau@aetsoft.net>
2023-04-03 02:19:49 +0200

    Add slider + 3 more screens

--- src/pages/Home/home.styled.ts
+++ src/pages/Home/home.styled.ts
@@ -3,6 +3,8 @@ import homebg from "../../images/home-bg.svg";
 import checked from "../../images/checked-icon.svg";
 import frame from "../../images/frame.svg";
 import framemobile from "../../images/frame-mobile.svg";
+import brain from '../../images/brain.svg';
+import brainMobile from '../../images/brain-mobile.svg';
 
 
 export const HomeScreenContainer = styled.div`
@@ -16,7 +18,7 @@ export const HomeBackground = styled.div`
   height: 627px;
   top: -140px;
   right: -140px;
-  background-image: url(${homebg});
+  background-image: url(${ homebg });
   background-repeat: no-repeat;
   background-size: cover;
   display: none;
@@ -35,7 +37,7 @@ export const HomeHeading = styled.h1`
   font-size: 80px;
   font-weight: 600;
   margin-top: 106px;
-  color: ${(props) => props.theme.colors.primaryBlack};
+  color: ${ (props) => props.theme.colors.primaryBlack };
   text-align: center;
   position: relative;
   z-index: 2;
@@ -51,7 +53,7 @@ export const HomeHeading = styled.h1`
 
 export const HomeHeadingItalic = styled.span`
   font-style: italic;
-  color: ${(props) => props.theme.colors.primaryViolet};
+  color: ${ (props) => props.theme.colors.primaryViolet };
 `;
 
 export const StartTrainingWrapper = styled.div<{ isHomeScreen?: boolean }>`
@@ -61,9 +63,15 @@ export const StartTrainingWrapper = styled.div<{ isHomeScreen?: boolean }>`
   align-items: center;
   margin-top: 32px;
   gap: 20px;
+  position: relative;
+  z-index: 2;
+
+  @media ( min-width: 320px) and (max-width: 744px) {
+    transform: translateX(0px);
+  }
 
   @media (max-width: 390px) {
-    display: ${(props) => (props.isHomeScreen ? "none" : "flex")};
+    display: ${ (props) => (props.isHomeScreen ? "none" : "flex") };
     position: -webkit-sticky;
     position: sticky;
     bottom: 0;
@@ -75,13 +83,13 @@ export const StartTrainingWrapper = styled.div<{ isHomeScreen?: boolean }>`
 
 export const StartTrainingButton = styled.button`
   text-align: center;
-  background-color: ${(props) => props.theme.colors.primaryViolet};
+  background-color: ${ (props) => props.theme.colors.primaryViolet };
   border: none;
   padding: 16px 110px;
   border-radius: 8px;
   font-size: 24px;
   font-weight: 500;
-  color: ${(props) => props.theme.colors.buttonTextWhite};
+  color: ${ (props) => props.theme.colors.buttonTextWhite };
   cursor: pointer;
   position: relative;
   z-index: 2;
@@ -95,7 +103,7 @@ export const StartTrainingButton = styled.button`
 export const Caption = styled.div`
   position: relative;
   z-index: 2;
-  color: ${(props) => props.theme.colors.defaultBlack};
+  color: ${ (props) => props.theme.colors.defaultBlack };
   font-size: 20px;
 
   @media (max-width: 390px) {
@@ -121,7 +129,7 @@ export const CardsContainer = styled.div`
 
 export const Card = styled.div`
   position: relative;
-  background: ${(props) => props.theme.colors.cardBackground};
+  background: ${ (props) => props.theme.colors.cardBackground };
   border-radius: 24px;
   padding: 16px 56px;
 
@@ -139,7 +147,7 @@ export const CardIcon = styled.div`
   left: 8px;
   top: 50%;
   transform: translateY(-50%);
-  background-image: url(${checked});
+  background-image: url(${ checked });
   width: 32px;
   height: 32px;
 
@@ -166,7 +174,7 @@ export const ImproveYourBrainWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  
+
   @media (max-width: 744px) {
     gap: 44px;
   }
@@ -192,7 +200,7 @@ export const ImproveYourBrainDescription = styled.div`
 export const ImproveTitle = styled.h2`
   font-size: 64px;
   text-align: center;
-  color: ${(props) => props.theme.colors.shadeOfGrey};
+  color: ${ (props) => props.theme.colors.shadeOfGrey };
   @media (max-width: 744px) {
     font-size: 36px;
   }
@@ -219,7 +227,7 @@ export const ImproveSubtitle = styled.h5`
   font-family: 'SF Pro Display';
   font-weight: 400;
   line-height: 48px;
-  color: ${(props) => props.theme.colors.shadeOfGrey};
+  color: ${ (props) => props.theme.colors.shadeOfGrey };
   @media (max-width: 744px) {
     font-size: 24px;
     line-height: 36px;
@@ -244,25 +252,26 @@ export const SubtitleSpan = styled.span`
 
 
 export const HowMusicalTraining = styled.div`
-  background-image: url(${frame});
+  background-image: url(${ frame });
   width: 1000px;
   height: 549px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-  
+  margin-top: 100px;
+
   @media (max-width: 744px) {
     width: 632px;
     height: 352px;
   }
-  
+
   @media (max-width: 620px) {
     width: 440px;
     height: 260px;
   }
-  
+
   @media (max-width: 390px) {
-    background-image: url(${framemobile});
+    background-image: url(${ framemobile });
     width: 342px;
     height: 198px;
   }
@@ -273,7 +282,7 @@ export const FoundersWrapper = styled.div`
   justify-content: space-between;
   gap: 94px;
   margin-top: 150px;
-  
+
   @media (max-width: 745px) {
     display: none;
   }
@@ -292,7 +301,7 @@ export const FounderDescription = styled.div`
 export const FounderPhoto = styled.div<{ photoUrl: string }>`
   width: 72px;
   height: 72px;
-  background-image: url(${(props) => props.photoUrl});
+  background-image: url(${ (props) => props.photoUrl });
   margin-top: 24px;
 `;
 export const FounderName = styled.div`
@@ -319,14 +328,407 @@ export const FounderSeparator = styled.div`
   right: -35px;
   width: 1px;
   height: 150px;
-  background-color: ${(props) => props.theme.colors.primaryBlack};
+  background-color: ${ (props) => props.theme.colors.primaryBlack };
   opacity: 0.3;
+
+  @media (min-width: 320px) and (max-width: 744px) {
+    display: none !important;
+  }
 `;
 
 export const CarouselWrapper = styled.div`
   display: none;
-  
+
+  @media (min-width: 320px) and (max-width: 744px) {
+    display: block;
+  }
+`;
+
+export const WorkoutContainer = styled.div`
+  background-color: ${ props => props.theme.colors.fullBodyBackground };
+  border-radius: 50px;
+  margin-top: 154px;
+  padding: 64px 32px;
+
+  @media (min-width: 320px) and (max-width: 390px) {
+    background-color: #FDFDFD;
+    padding: 0;
+    margin-top: 96px;
+  }
+`;
+
+export const WorkoutHeading = styled.h1`
+  text-align: center;
+  font-family: 'SF Pro Display';
+  font-style: italic;
+  font-weight: 800;
+  font-size: 64px;
+  color: ${ props => props.theme.colors.primaryBlack };
+
   @media (max-width: 744px) {
-    display: block
+    font-size: 36px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 24px;
+  }
+`;
+
+export const WorkoutSubheading = styled.h1`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 64px;
+
+  @media (max-width: 744px) {
+    font-size: 36px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 24px;
+    display: inline;
+  }
+`;
+
+export const WorkoutDescription = styled.p`
+  text-align: center;
+  color: ${ props => props.theme.colors.primaryBlack };
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  margin-top: 32px;
+
+  @media (max-width: 744px) {
+    font-size: 24px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 16px;
+  }
+`;
+
+export const WorkoutBrainAndList = styled.div`
+  margin-top: 64px;
+  display: flex;
+  gap: 44px;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  @media (min-width: 320px) and (max-width: 1000px) {
+    justify-content: center;
+  }
+`;
+
+export const WorkoutBrainImage = styled.div`
+  background-image: url(${ brain });
+  width: 350px;
+  height: 306px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+
+  @media (min-width: 320px) and (max-width: 390px) {
+    background-image: url(${ brainMobile });
+    width: 336px;
+    height: 294px;
+  }
+`;
+
+export const WorkoutList = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 484px;
+`;
+
+export const WorkoutListItem = styled.div`
+  margin-bottom: 28px;
+  position: relative;
+  word-break: break-word;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+
+  @media (min-width: 320px) and (max-width: 390px) {
+    margin-left: 40px;
+  }
+`;
+
+export const WorkoutListItemNumber = styled.div<{ color: string }>`
+  height: 44px;
+  width: 44px;
+  background-color: #fff;
+  color: ${ props => props.color ?? '#000' };
+  border-radius: 50%;
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.1);
+  font-family: 'SF Pro Display';
+  position: absolute;
+  left: -66px;
+  top: -5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 320px) and (max-width: 390px) {
+    top: -3px;
+    left: -42px;
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const WorkoutListItemText = styled.div`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  color: #212121;
+
+  @media (min-width: 320px) and (max-width: 390px) {
+    font-size: 16px;
+  }
+`;
+
+export const UsingMusicContainer = styled.div`
+  border-radius: 50px;
+  margin-top: 150px;
+
+  @media (min-width: 320px) and (max-width: 390px) {
+    background-color: #FDFDFD;
+    padding: 0;
+    margin-top: 96px;
+  }
+`;
+
+export const UsingMusicHeading = styled.h1`
+  text-align: center;
+  font-family: 'SF Pro Display';
+  font-style: italic;
+  font-weight: 800;
+  font-size: 64px;
+  color: ${ props => props.theme.colors.primaryBlack };
+
+  @media (max-width: 744px) {
+    font-size: 36px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 24px;
+  }
+`;
+
+export const UsingMusicSubheading = styled.h1`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 64px;
+
+  @media (max-width: 744px) {
+    font-size: 36px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 24px;
+    display: inline;
+  }
+`;
+
+export const UsingMusicDescription = styled.p`
+  text-align: center;
+  color: ${ props => props.theme.colors.primaryBlack };
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  margin-top: 32px;
+
+  @media (max-width: 744px) {
+    font-size: 24px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 16px;
+  }
+`;
+
+export const UsingMusicItems = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 24px;
+  margin-top: 64px;
+  
+  @media (min-width: 320px) and (max-width: 564px) {
+    justify-content: center;
+    flex-direction: column;
+  }
+  
+  @media (min-width: 320px) and (max-width: 979px) {
+    gap: 16px;
+  }
+`;
+
+export const UsingMusicItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 55px;
+  flex-wrap: wrap;
+  background-color: #f8f8f8;
+  gap: 32px;
+  border-radius: 24px;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    flex-direction: column;
+    padding: 16px;
+    gap: 16px;
+  }
+  
+  @media (min-width: 320px) and (max-width: 564px) {
+    flex-direction: column;
+    align-items: center;
+    padding: 16px;
+  }
+`;
+
+export const UsingMusicItemImage = styled.div<{ imagePath: string, tabletImagePath: string }>`
+  width: 71px;
+  height: 71px;
+  background-image: url(${ props => props.imagePath });
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    background-image: url(${ props => props.tabletImagePath });
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+export const UsingMusicItemTextWrapper = styled.div`
+  max-width: 265px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    align-items: center;
+    max-width: 276px;
+    text-align: center;
+  }
+
+  @media (min-width: 320px) and (max-width: 564px) {
+    flex-direction: column;
+    align-items: center;
+    max-width: 308px;
+  }
+`;
+export const UsingMusicItemHeading = styled.h4`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+  }
+
+  @media (min-width: 320px) and (max-width: 564px) {
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    text-align: center;
+  }
+`;
+export const UsingMusicItemDescription = styled.div`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+  }
+
+  @media (min-width: 320px) and (max-width: 564px) {
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    text-align: center;
+  }
+`;
+
+export const IsBraintuneContainer = styled.div`
+  padding: 64px 32px;
+  background-color: #F9F6FF;
+  border-radius: 50px;
+  margin-top: 154px;
+
+  @media (min-width: 320px) and (max-width: 564px) {
+    padding: 32px 24px;
+    border-radius: 16px;
+  }
+`;
+
+export const IsBraintuneHeading = styled.h1`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 64px;
+  color: ${props => props.theme.colors.primaryBlack};
+  text-align: center;
+  
+  @media (min-width: 565px) and (max-width: 979px) {
+    font-size: 36px;
+  }
+  
+  @media (min-width: 320px) and (max-width: 564px) {
+    font-size: 24px;
+  }
+`;
+
+export const IsBraintuneSubheading = styled.div`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  color: ${props => props.theme.colors.primaryBlack};
+  margin-top: 64px;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    font-size: 24px;
+  }
+
+  @media (min-width: 320px) and (max-width: 564px) {
+    font-size: 20px;
+  }
+`;
+
+export const IsBraintuneParagraph = styled.p`
+  font-family: 'SF Pro Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  color: ${props => props.theme.colors.primaryBlack};
+  margin-top: 24px;
+
+  @media (min-width: 565px) and (max-width: 979px) {
+    font-size: 20px;
+  }
+
+  @media (min-width: 320px) and (max-width: 564px) {
+    font-size: 16px;
   }
 `;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,10 +6,12 @@ import {
   CardDescription,
   CardIcon,
   CardsContainer,
-  CardTitle, CarouselWrapper,
+  CardTitle,
+  CarouselWrapper,
   FounderDescription,
   FounderItem,
-  FounderJob, FounderName,
+  FounderJob,
+  FounderName,
   FounderPhoto,
   FounderPosition,
   FounderSeparator,
@@ -27,20 +29,47 @@ import {
   StartTrainingWrapper,
   SubtitleSpan,
   TitleSpan,
+  WorkoutBrainImage,
+  WorkoutBrainAndList,
+  WorkoutContainer,
+  WorkoutDescription,
+  WorkoutHeading,
+  WorkoutList,
+  WorkoutListItem,
+  WorkoutListItemNumber,
+  WorkoutListItemText,
+  WorkoutSubheading,
+  UsingMusicContainer,
+  UsingMusicHeading,
+  UsingMusicSubheading,
+  UsingMusicDescription,
+  UsingMusicItems,
+  UsingMusicItem,
+  UsingMusicItemImage,
+  UsingMusicItemTextWrapper,
+  UsingMusicItemHeading,
+  UsingMusicItemDescription, IsBraintuneContainer, IsBraintuneHeading, IsBraintuneSubheading, IsBraintuneParagraph,
 } from "./home.styled";
 import founder1 from '../../images/founder-1.svg';
 import founder2 from '../../images/founder-2.svg';
 import founder3 from '../../images/founder-3.svg';
-import Slider, { Settings } from 'react-slick';
+import cardBrain from '../../images/card-brain.svg';
+import cardBulb from '../../images/card-bulb.svg';
+import cardCreativity from '../../images/card-creativity.svg';
+import cardHand from '../../images/card-hand.svg';
+import cardPerson from '../../images/card-person.svg';
+import cardEar from '../../images/card-ear.svg';
+import cardBrain40 from '../../images/card-brain-40.svg';
+import cardBulb40 from '../../images/card-bulb-40.svg';
+import cardCreativity40 from '../../images/card-creativity-40.svg';
+import cardHand40 from '../../images/card-hand-40.svg';
+import cardPerson40 from '../../images/card-person-40.svg';
+import cardEar40 from '../../images/card-ear-40.svg';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const settings: Settings = {
-  dots: true,
-  infinite: true,
-  arrows: false,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
-};
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Navigation, Pagination } from "swiper";
 
 interface IFounder {
   id: number,
@@ -52,6 +81,52 @@ interface IFounder {
 }
 
 const Home: FC = () => {
+  const StartScreen = () => {
+    const cards = [
+      {
+        id: 1,
+        label: "Research-based",
+        description: "Backed by latest scientific findings",
+      },
+      {
+        id: 2,
+        label: "Personalized",
+        description: "Adapts to your goals and progress",
+      },
+      {
+        id: 3,
+        label: "For every age",
+        description: "It’s never too late to get into music",
+      },
+    ];
+
+    return (
+      <HomeScreenContainer>
+        <HomeHeading>
+          Keep your <HomeHeadingItalic>mind young</HomeHeadingItalic> and sharp
+          through <HomeHeadingItalic>music</HomeHeadingItalic>
+        </HomeHeading>
+        <StartTrainingWrapper isHomeScreen>
+          <StartTrainingButton>Start training</StartTrainingButton>
+          <Caption>🎵 no music instrument or training needed</Caption>
+        </StartTrainingWrapper>
+        <HomeBackground/>
+
+        <CardsContainer>
+          { cards.map((item) => {
+            return (
+              <Card key={ item.id }>
+                <CardIcon/>
+                <CardTitle>{ item.label }</CardTitle>
+                <CardDescription>{ item.description }</CardDescription>
+              </Card>
+            );
+          }) }
+        </CardsContainer>
+      </HomeScreenContainer>
+    );
+  };
+
   const ImproveYourBrain = () => {
     const founders: Array<IFounder> = [
       {
@@ -118,69 +193,228 @@ const Home: FC = () => {
         </FoundersWrapper>
 
         <CarouselWrapper>
-          <Slider {...settings}>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-          </Slider>
+          <Swiper
+            slidesPerView={ 1 }
+            spaceBetween={ 40 }
+            navigation
+            centeredSlides
+            modules={ [Autoplay, Pagination, Navigation] }
+            pagination={ { clickable: true } }
+            scrollbar={ { draggable: true } }
+            className='swiper'
+            autoplay={ {
+              delay: 2500,
+              disableOnInteraction: false,
+            } }
+          >
+            { founders.map((el, index) => {
+              return (
+                <SwiperSlide className='swiper-slider-item'>
+                  <FounderItem key={ el.id }>
+                    <FounderDescription>
+                      { el.description }
+                    </FounderDescription>
+                    <FounderPhoto photoUrl={ el.photoUrl }/>
+                    <FounderName>
+                      { el.name }
+                    </FounderName>
+                    <FounderPosition>{ el.position }</FounderPosition>
+                    <FounderJob>{ el.job }</FounderJob>
+                    <FounderSeparator style={ {
+                      display: index === founders.length - 1 ? 'none' : 'block'
+                    } }/>
+                  </FounderItem>
+                </SwiperSlide>
+              )
+            }) }
+          </Swiper>
         </CarouselWrapper>
 
-        <StartTrainingWrapper isHomeScreen style={ {
-          transform: 'translateX(-30px)'
-        } }>
+        <StartTrainingWrapper isHomeScreen className='transformX30'>
           <StartTrainingButton>Start training</StartTrainingButton>
           <Caption>🎵 no music instrument or training needed</Caption>
         </StartTrainingWrapper>
         <HomeBackground/>
       </ImproveYourBrainWrapper>
-  );
+    );
   };
 
-  const StartScreen = () => {
-    const cards = [
+  const WorkoutForBrain = () => {
+    const workoutList = [
       {
         id: 1,
-        label: "Research-based",
-        description: "Backed by latest scientific findings",
+        color: '#008DAB',
+        text: 'Planning • Decision making • Logic • Speech'
       },
       {
         id: 2,
-        label: "Personalized",
-        description: "Adapts to your goals and progress",
+        color: '#38629D',
+        text: 'Memory • Stress Tolerance • Emotional behavior and motivation'
       },
       {
         id: 3,
-        label: "For every age",
-        description: "It’s never too late to get into music",
+        color: '#B290FF',
+        text: 'Auditory perception and processing'
       },
-    ];
+      {
+        id: 4,
+        color: '#008DAB',
+        text: 'Touch Perception • Language Processing'
+      },
+      {
+        id: 5,
+        color: '#999999',
+        text: 'Coordination between hemispheres, sensory and motor areas'
+      },
+      {
+        id: 6,
+        color: '#B290FF',
+        text: 'Visual perception and processing'
+      },
+      {
+        id: 7,
+        color: '#B290FF',
+        text: 'Motor learning and coordination'
+      }
+    ]
 
     return (
-      <HomeScreenContainer>
-        <HomeHeading>
-          Keep your <HomeHeadingItalic>mind young</HomeHeadingItalic> and sharp
-          through <HomeHeadingItalic>music</HomeHeadingItalic>
-        </HomeHeading>
+      <WorkoutContainer>
+        <WorkoutHeading>
+          <WorkoutSubheading>“Full body” workout</WorkoutSubheading>{ ' ' }
+          for your brain
+        </WorkoutHeading>
+        <WorkoutDescription>
+          Actively engaging in music is like hitting the gym but for your brain. It stimulates virtually all areas of
+          your brain at once, like no other activity.
+        </WorkoutDescription>
+
+        <WorkoutBrainAndList>
+          <WorkoutBrainImage/>
+          <WorkoutList>
+            {
+              workoutList.map((el) => {
+                return (
+                  <WorkoutListItem key={ el.id }>
+                    <WorkoutListItemNumber color={ el.color }>
+                      { el.id }
+                    </WorkoutListItemNumber>
+                    <WorkoutListItemText>{ el.text }</WorkoutListItemText>
+                  </WorkoutListItem>
+                )
+              })
+            }
+          </WorkoutList>
+        </WorkoutBrainAndList>
+      </WorkoutContainer>
+    )
+  };
+
+  const UsingMusic = () => {
+    const items = [
+      {
+        id: 1,
+        imagePath: cardBrain,
+        tabletImagePath: cardBrain40,
+        title: 'Neural Plasticity',
+        description: 'Become better at learning new skills and adapting to challenges.'
+      },
+      {
+        id: 2,
+        imagePath: cardBulb,
+        tabletImagePath: cardBulb40,
+        title: 'Cognitive Abilities',
+        description: 'Boost your attention, memory, focus, and problem solving skills. '
+      },
+      {
+        id: 3,
+        imagePath: cardEar,
+        tabletImagePath: cardEar40,
+        title: 'Hearing',
+        description: 'Mitigate auditory decline, improve your hearing and comprehension.'
+      },
+      {
+        id: 4,
+        imagePath: cardPerson,
+        tabletImagePath: cardPerson40,
+        title: 'Mental Well-Being',
+        description: 'Enhance your mood, relieve stress and anxiety. Harness your emotions.'
+      },
+      {
+        id: 5,
+        imagePath: cardCreativity,
+        tabletImagePath: cardCreativity40,
+        title: 'Creativity and Motivation',
+        description: 'Enhance your out-of-the box thinking and tap into your inspiration.'
+      },
+      {
+        id: 6,
+        imagePath: cardHand,
+        tabletImagePath: cardHand40,
+        title: 'Motor Skills',
+        description: 'Better your dexterity and hand-eye coordination.'
+      },
+    ]
+
+    return (
+      <UsingMusicContainer>
+        <UsingMusicHeading>
+          <UsingMusicSubheading>Using Music For</UsingMusicSubheading>{ ' ' }
+          Non-Musical Goals
+        </UsingMusicHeading>
+        <UsingMusicDescription>
+          BrainTune’s training program is expertly designed to engage key areas of your brain through active
+          participation in music. This helps build new neural connections improving your abilities way beyond the
+          musical domain.
+        </UsingMusicDescription>
+        <UsingMusicItems>
+          { items.map(el => {
+            return (
+              <UsingMusicItem key={ el.id }>
+                <UsingMusicItemImage imagePath={ el.imagePath } tabletImagePath={ el.tabletImagePath }/>
+                <UsingMusicItemTextWrapper>
+                  <UsingMusicItemHeading>
+                    { el.title }
+                  </UsingMusicItemHeading>
+                  <UsingMusicItemDescription>
+                    { el.description }
+                  </UsingMusicItemDescription>
+                </UsingMusicItemTextWrapper>
+              </UsingMusicItem>
+            )
+          }) }
+        </UsingMusicItems>
         <StartTrainingWrapper isHomeScreen>
-          <StartTrainingButton>Start training</StartTrainingButton>
+          <StartTrainingButton>Choose your goals</StartTrainingButton>
           <Caption>🎵 no music instrument or training needed</Caption>
         </StartTrainingWrapper>
         <HomeBackground/>
-
-        <CardsContainer>
-          { cards.map((item) => {
-            return (
-              <Card key={ item.id }>
-                <CardIcon/>
-                <CardTitle>{ item.label }</CardTitle>
-                <CardDescription>{ item.description }</CardDescription>
-              </Card>
-            );
-          }) }
-        </CardsContainer>
-      </HomeScreenContainer>
-    );
+      </UsingMusicContainer>
+    )
   };
+
+  const IsBraintuneForMe = () => {
+    return (
+      <IsBraintuneContainer>
+        <IsBraintuneHeading>
+          Is <i>BrainTune</i> for me?
+        </IsBraintuneHeading>
+
+        <IsBraintuneSubheading>
+          What if I’m not ‘musical’ enough?
+        </IsBraintuneSubheading>
+
+        <IsBraintuneParagraph>
+          If you’ve ever tapped your foot to a song, you’re good to go.
+        </IsBraintuneParagraph>
+
+        <IsBraintuneSubheading>No instrument? No problem!</IsBraintuneSubheading>
+
+        <IsBraintuneParagraph>We have you covered with plenty of virtual instruments to jam with, as well as a range of
+          clapping, vocal, and active listening activities to get into.</IsBraintuneParagraph>
+      </IsBraintuneContainer>
+    )
+  }
 
   return (
     <CommonContainer
@@ -190,8 +424,11 @@ const Home: FC = () => {
     >
       <StartScreen/>
       <ImproveYourBrain/>
+      <WorkoutForBrain/>
+      <UsingMusic/>
+      <IsBraintuneForMe/>
     </CommonContainer>
   );
-  };
+};
 
-  export default Home;
+export default Home;
